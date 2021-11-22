282cf2ea6c75da644f2327c6bd508a16f970f38d
AmroFarouk <amr.faroukk11@gmail.com>
2021-11-22 11:35:00 +0200

    rebuild with framework 4.6

--- digital_Library/modals/Model1.Designer.cs
+++ digital_Library/modals/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\amrfa\Source\Repos\HindMahmoud\digital_Library\digital_Library\modals\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\amrfa\source\repos\HindMahmoud\digital_Library\digital_Library\modals\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
